9b71c4200e0c42aad9302593bb39bc8e2d76c51b
Ilya <blacklord@list.ru>
2023-05-29 01:24:04 +1000

    Добавил объекты обработки таблицы запросов "request"

---
+++ database/api/requests.py
@@ -0,0 +1,34 @@
+def get_requests(mongo, ):
+    documents = mongo.db.requests.find()
+    output = []
+    for document in documents:
+        output.append({
+            '_id': str(document['_id']),
+            'chat_id': document['chat_id'],
+            'response_text': document['response_text'],
+        })
+    return output
+
+def get_request(mongo, id):
+    document = mongo.db.requests.find_one({'_id': id})
+    
+    if document:
+        return document
+    else:
+        return None
+
+def create_request(mongo, request):
+    result = mongo.db.requests.insert_one(request)
+    return str(result.inserted_id)
+
+def update_request(mongo, id, data):
+    document = mongo.db.requests.find_one({'_id': id})
+    if document:
+        mongo.db.requests.update_one({'_id': id}, {'$set': data})
+        return True
+    else:
+        return False
+
+def delete_request(mongo, id):
+    result = mongo.db.requests.delete_one({'_id': id})
+    return result.deleted_count > 0
